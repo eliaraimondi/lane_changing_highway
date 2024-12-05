f3d0befd4f7e4f147833a1ea0b2a05c9528788f0
eliaraimondi <elioraimond02@gmail.com>
2024-12-05 18:44:32 +0000

    Calcolo di X_k+1

--- src/pdm4ar/exercises/ex12/agent.py
+++ src/pdm4ar/exercises/ex12/agent.py
@@ -1,6 +1,7 @@
 import random
 from dataclasses import dataclass
 from typing import Sequence
+import numpy as np
 
 from commonroad.scenario.lanelet import LaneletNetwork
 from dg_commons import PlayerName
@@ -8,7 +9,7 @@ from dg_commons.sim.goals import PlanningGoal
 from dg_commons.sim import SimObservations, InitSimObservations
 from dg_commons.sim.agents import Agent
 from dg_commons.sim.models.obstacles import StaticObstacle
-from dg_commons.sim.models.vehicle import VehicleCommands
+from dg_commons.sim.models.vehicle import VehicleCommands, VehicleModel, VehicleState
 from dg_commons.sim.models.vehicle_structures import VehicleGeometry
 from dg_commons.sim.models.vehicle_utils import VehicleParameters
 
@@ -41,6 +42,10 @@ class Pdm4arAgent(Agent):
         self.sg = init_obs.model_geometry
         self.sp = init_obs.model_params
 
+        lanelet_network = init_obs.dg_scenario.lanelet_network
+        control_points = init_obs.goal.ref_lane.control_points
+        starting_lanelet_id = lanelet_network.find_lanelet_by_position([control_points[1].q.p])[0][0]
+
     def get_commands(self, sim_obs: SimObservations) -> VehicleCommands:
         """This method is called by the simulator every dt_commands seconds (0.1s by default).
         Do not modify the signature of this method.
@@ -50,9 +55,18 @@ class Pdm4arAgent(Agent):
         :param sim_obs:
         :return:
         """
-
         # todo implement here some better planning
         rnd_acc = random.random() * self.params.param1
         rnd_ddelta = (random.random() - 0.5) * self.params.param1
+
+        current_car = VehicleModel.default_car(sim_obs.players[self.name].state)
+
+        X_k1 = sim_obs.players[self.name].state + 0.1 * current_car.dynamics(
+            x0=sim_obs.players[self.name].state,
+            u=VehicleCommands.from_array(np.array([rnd_acc, rnd_ddelta])),
+        )
+
+        print(f"Current state: {sim_obs.players[self.name].state}")
+        print(f"Next state: {X_k1}")
 
         return VehicleCommands(acc=rnd_acc, ddelta=rnd_ddelta)
